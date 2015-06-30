82f7777078193f597baf81d815d83a3865b9727d
greeve <greg_reeve@byu.edu>
2015-06-29 22:12:42 -0600

    Add functions to demonstrate how to use the Math.sin function to move, blink, and change the size of a circle.

--- trig02.js
+++ trig02.js
@@ -6,14 +6,28 @@ window.onload = function() {
 
     var centerY = height * 0.5,
         centerX = width * 0.5,
+        baseRadius = 100,
         baseAlpha = 0.5,
         offset = 0.5,
         speed = 0.1,
         angle = 0;
 
-    render()
+    // move();
+    // blink();
+    grow();
 
-    function render() {
+    function move() {
+        var y = centerY + Math.sin(angle) * (offset * height);
+        context.fillStyle = "rgba(0, 0, 255, 0.5)";
+        context.clearRect(0, 0, width, height);
+        context.beginPath();
+        context.arc(centerX, y, 50, 0, Math.PI * 2, false);
+        context.fill();
+        angle += speed;
+        requestAnimationFrame(move);
+    };
+
+    function blink() {
         var alpha = baseAlpha + Math.sin(angle) * offset;
         context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
         context.clearRect(0, 0, width, height);
@@ -21,6 +35,17 @@ window.onload = function() {
         context.arc(centerX, centerY, 100, 0, Math.PI * 2, false);
         context.fill();
         angle += speed;
-        requestAnimationFrame(render);
+        requestAnimationFrame(blink);
+    };
+
+    function grow() {
+        var radius = baseRadius + Math.sin(angle) * (offset + 45.5);
+        context.fillStyle = "rgba(0, 150, 0, 0.75)";
+        context.clearRect(0, 0, width, height);
+        context.beginPath();
+        context.arc(centerX, centerY, radius, 0, Math.PI * 2, false);
+        context.fill();
+        angle += speed;
+        requestAnimationFrame(grow);
     }
 }
